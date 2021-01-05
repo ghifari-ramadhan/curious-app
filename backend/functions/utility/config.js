07f772e0e6bc92ec632c515d7288931e3d33da08
ghifari-ramadhan <71369040+ghifari-ramadhan@users.noreply.github.com>
2021-01-05 10:57:09 +0700

    Initial Setup

---
+++ backend/functions/utility/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyB56bd6HDIwps9MyM4vza4M6hhpd5o4Sg4",
+    authDomain: "insvire-curious-app.firebaseapp.com",
+    projectId: "insvire-curious-app",
+    storageBucket: "insvire-curious-app.appspot.com",
+    messagingSenderId: "32784977098",
+    appId: "1:32784977098:web:66ff9a0b1a3d4c00457833",
+    measurementId: "G-6VPVBYFZZW"
+}
